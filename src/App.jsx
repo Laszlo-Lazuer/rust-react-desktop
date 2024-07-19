f4507d87627f881bbc7e18a80015fec85b986d57
Laszlo Lazuer <visualjazz@gmail.com>
2024-07-18 22:10:54 -0600

    chore: update readmpe and bump version.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import UpdateChecker from './components/UpdateChecker';
 import ImageEditor from './components/ImageEditor';
 import "./App.scss";
-import { Flex, Text, Button } from '@radix-ui/themes';
 
 function App() {
 
